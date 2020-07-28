97c52731a619168fcb69ddfeca937657338a0132
Evgeny Kalinin <krampuscompany@gmail.com>
2020-07-28 15:42:56 +0700

    Add price parsing

--- dns/spiders/dns_ultrabook.py
+++ dns/spiders/dns_ultrabook.py
@@ -13,43 +13,58 @@ class DnsUltrabookSpider(scrapy.Spider):
                        'FEED_FORMAT': 'csv'}
 
     def parse(self, response):
-        print("procesing:" + response.url)
         # Give data of css
         product_name = response.css('.product-info__title-link > a::text').extract()
         # Example: technical parameters
         # [1366x768, SVA (TN+film), Intel Pentium 4417U, 2 х 2.3 ГГц, RAM 4 ГБ,
         #  SSD 128 ГБ, Intel HD 610 , Wi-Fi, Windows 10 Home]
         product_info = response.css('.product-info__title-description::text').extract()
+        product_url = response.css('.product-info__title-link > a::attr(href)').extract()
+        full_product_url = []
+        for url in product_url:
+            full_product_url.append("https://www.dns-shop.ru" + url)
+        for i in range(len(full_product_url)):
+            try:
+                yield scrapy.Request(full_product_url[i],
+                                     callback=self.price_parse,
+                                     errback=self.errback_page2,
+                                     cb_kwargs=dict(product_name=product_name[i],
+                                                    product_info=product_info[i],
+                                                    product_url=full_product_url[i]))
+            except IndexError:
+                print("ERROR:", product_name[i])
 
-        row_data = zip(product_name, product_info)
-
-        for item in row_data:
-            # Create dictionary
-            info = item[1].split(", ")
-            scraped_info = {
-                'page': response.url,
-                'product_name': item[0],
-                'matrix': info[1],
-                'processor': info[2],
-                'core': info[3],
-                'ram': info[4],
-                'ssd': info[5],
-                'card': info[6]
-            }
-
-            # Generate Information for scraping
-            yield scraped_info
-
+        print(self.page)
         # Pagination
         # Create Last page
         if not self.last_page:
             self.last_page = response.css('.pagination-widget__page::attr(data-page-number)').extract()[-1]
+            print(self.last_page)
         # Exit if scrap last page
         if int(self.last_page) == int(self.page):
-            yield scraped_info
+            raise scrapy.exceptions.CloseSpider('bandwidth_exceeded')
         else:
             self.page += 1
-            next_page = 'https://www.dns-shop.ru/catalog/17a892f816404e77/?f[65c]=264d&p=%s' % self.page
+            next_page = 'https://www.dns-shop.ru/catalog/17a892f816404e77/?f[65c]=264d&p=' + self.page
             if next_page:
                 yield scrapy.Request(response.urljoin(next_page),
                                      callback=self.parse)
+
+    def price_parse(self, response, product_name, product_info, product_url):
+        price = response.css(".current-price-value::attr(data-price-value)").extract_first()
+        info = product_info.split(", ")
+        scrap_info = {
+            'product_name': product_name,
+            'product_url': product_url,
+            'price': price,
+            'matrix': info[1],
+            'processor': info[2],
+            'core': info[3],
+            'ram': info[4],
+            'ssd': info[5],
+            'card': info[6],
+        }
+        return scrap_info
+
+    def errback_page2(self, failure):
+        yield dict(main_url=failure.request.cb_kwargs['product_url'])
